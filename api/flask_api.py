c4550ace4c39c6de6a456ddb495627848788e111
balusubhanuprakash <balusubhanuprakash091@gmail.com>
2025-05-23 10:52:36 +0530

    trying render

--- api/flask_api.py
+++ api/flask_api.py
@@ -1,6 +1,7 @@
 # api/tokenize.py
 from flask import Flask, request, jsonify
 from nltk.tokenize import TreebankWordTokenizer
+import os
 
 app = Flask(__name__)
 tokenizer = TreebankWordTokenizer()
@@ -12,3 +13,7 @@ def tokenize_text():
     text = data.get("text", "")
     tokens = tokenizer.tokenize(text)
     return jsonify({"tokens": tokens})
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # default to 5000 if not set
+    app.run(host='0.0.0.0', port=port)
